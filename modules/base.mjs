99ad97272e6f63448068cdc48f5cffcf6da475b6
Steven Brcak <sbrcak@live.com>
2023-04-06 20:08:32 -0700

    Point before Andrei's help.

--- modules/base.mjs
+++ modules/base.mjs
@@ -17,8 +17,8 @@ class Board {
         this.scoreUpdater();
     }
     boardBuilder (size) {
-        let xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let yAxis = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+        let xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //10 values 0-9 index
+        let yAxis = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']; ////10 0-9 index
         //for-loop from the two values above to create new boards ranges
         let alpha = [];
         let numeric = [];
@@ -27,7 +27,7 @@ class Board {
         for (let i = 0; i < size; i++){
             alpha.push(xAxis[i]);
         }
-        for (let i = 0; i <= size; i++){
+        for (let i = 0; i < size; i++){
             numeric.push(yAxis[i]);
         }
         for (const number of numeric){
@@ -39,11 +39,15 @@ class Board {
     }
     nodeGatherer(battleshipNodes) {
         let occupiedNodes = new Set; //derived from Battleships
+        // for (let i = 0; i < battleshipNodes.length; i++){
+        //     for (let j = 0; j < battleshipNodes[i].length; j++){
+        //         occupiedNodes.add(battleshipNodes[i][j]);
+        //     }
+        // } 
         for (let i = 0; i < battleshipNodes.length; i++){
-            for (let j = 0; j < battleshipNodes[i].length; j++){
-                occupiedNodes.add(battleshipNodes[i][j]);
-            }
+            occupiedNodes.add(battleshipNodes[i]);
         } 
+        // this.occupiedNodes = occupiedNodes;
         this.occupiedNodes = occupiedNodes;
     }
     nodeGuesser (guess) {
@@ -71,68 +75,83 @@ class Board {
 }
 
 class Battleship {
-    constructor(ID, shipSize, boardSize, board) {
+    constructor(ID, shipSize, board) {
         this.ID = ID; //track ship amounts
         this.shipSize = shipSize; //track ship sizes
-        this.boardSize = boardSize; //this is a number from the other class
+        //this.boardSize = boardSize; //this is a number from the other class
         this.board = board; //get the fullBoard
-        this.positionSetter(shipSize, boardSize, board) //track ship nodes. BoardSize is a number, board is a property
+        this.positionSetter(shipSize, board) //track ship nodes. BoardSize is a number, board is a property
     }
     //horizontal ship => increment less than Letter-Number(less than size)
     //vertical ship => increment + size
-    positionSetter(shipSize, boardSize, board){ //board size is derived from the size set by the board => Board.fullBoard
+    positionSetter(shipSize, board) { //board size is derived from the size set by the board => Board.fullBoard
+        //old params (shipSize, boardSize, fullBoard)
+
+        let openNodes = this.board.fullBoard;
+        let boardSize = this.board.size;
+
         let orientationRoll = Math.floor(Math.random() * 2);
-        //console.log('Orientation Roll: ', orientationRoll );
-        let openNodes = board;
         let initialNodeIndex = Math.floor(Math.random() * openNodes.length);
-        //console.log('INITIAL INDEX:', initialNodeIndex);
-        let occupiedNodes = []; //[openNodes[initialNodeIndex]];
+        
+        let occupiedNodes = []; 
         //occupiedNodes has to be passed back to the original board
 
         this.initialNode = openNodes[initialNodeIndex];
         let numericNode = Number(this.initialNode.slice(0,-1));
-        console.log('Numeric NODE: ', numericNode);
-        if( orientationRoll === 1 ){ //vertical placement. Needs conditions for space
-            if((initialNodeIndex + (boardSize * shipSize)) <= board.length){ //vertical placement DOWN
-                for(let i = 0; i < shipSize; i++) {
-                    occupiedNodes.push(openNodes[initialNodeIndex + (i * boardSize)]);
-                    //nodeTracker.add(openNodes[initialNodeIndex + (i * boardSize)]);
+        console.log('INITIAL NODE: ', this.initialNode);
+        console.log('SHIP SIZE: ', shipSize);
+        console.log('OCCUPIED NODES LENGTH: ', occupiedNodes.length);
+        while (occupiedNodes.length < shipSize) {
+            if (orientationRoll === 1) { //vertical placement. 
+                let condition = initialNodeIndex + (boardSize * shipSize);
+                let counterCondition = [];
+                //old param condition <= board.length
+                if(condition <= openNodes.length){ //vertical placement DOWN
+                    for(let i = 0; i < shipSize; i++) {
+                        occupiedNodes.push(openNodes[initialNodeIndex + (i * boardSize)]);
+                    }
+                    console.log('VERTICAL DOWN TRIGGER');
+                    this.nodes = occupiedNodes;
+                    this.board.nodeGatherer(occupiedNodes);
+    
+                } else if (condition >= 0 ) { //vertical placement UP. Check for space
+                    for(let i = 0; i < shipSize; i++) {
+                        occupiedNodes.push(openNodes[initialNodeIndex - (i * boardSize)]);
+                    }
+                    console.log('VERTICAL UP TRIGGER');
+                    this.nodes = occupiedNodes;
+                    this.board.nodeGatherer(occupiedNodes);
+                } else {
+                    console.log('ABORT VERTICAL TRIGGER OPERATION');
+                    orientationRoll = 0;
+                    continue;
                 }
-                console.log('VERTICAL DOWN TRIGGER');
-                this.nodes = occupiedNodes;
-
-            } else if ((initialNodeIndex - (boardSize * shipSize)) >= 0 ) { //vertical placement UP. Check for space
-                for(let i = 0; i < shipSize; i++) {
-                    occupiedNodes.push(openNodes[initialNodeIndex - (i * boardSize)]);
-                    //nodeTracker.add(openNodes[initialNodeIndex + (i * boardSize)]);
-                }
-                console.log('VERTICAL UP TRIGGER');
-                this.nodes = occupiedNodes;
-            } else {
-                console.log('ABORT VERTICAL TRIGGER OPERATION');
-                orientationRoll = 0;
             }
-
-        } else { //horizontal
-            //instead of numericNode, change to initialIndex? Or, maybe -1 to account for value
-            let condition = boardSize - (shipSize + numericNode);
-            if(condition > 0) { //horizontal right
-                for(let i = 0; i < shipSize; i++){
-                    occupiedNodes.push(openNodes[initialNodeIndex + i]);
-
-                    //nodeTracker.add(openNodes[initialNodeIndex + (i * boardSize)]);
+            else { //horizontal
+                //instead of numericNode, change to initialIndex? Or, maybe -1 to account for value
+                let condition = boardSize - (shipSize + numericNode);
+    
+                if(condition > 0) { //horizontal right
+                    for(let i = 0; i < shipSize; i++){
+                        occupiedNodes.push(openNodes[initialNodeIndex + i]);
+                    }
+                    console.log('HORIZONTAL TRIGGER RIGHT');
+                    this.nodes = occupiedNodes;
+                    this.board.nodeGatherer(occupiedNodes);
                 }
-                console.log('HORIZONTAL TRIGGER RIGHT');
-                this.nodes = occupiedNodes;
-            }
-            else if(condition < 0 ) { //horizontal left IS NOT TRIGGERING
-                for(let i = 0; i < shipSize; i++){
-                    occupiedNodes.push(openNodes[initialNodeIndex - i]);
-
-                    //nodeTracker.add(openNodes[initialNodeIndex + (i * boardSize)]);
+                else if(condition < 0 ) { //horizontal left IS NOT TRIGGERING
+                    for(let i = 0; i < shipSize; i++){
+                        occupiedNodes.push(openNodes[initialNodeIndex - i]);
+                    }
+                    console.log('HORIZONTAL TRIGGER LEFT');
+                    this.nodes = occupiedNodes;
+                    this.board.nodeGatherer(occupiedNodes);
                 }
-                console.log('HORIZONTAL TRIGGER LEFT');
-                this.nodes = occupiedNodes;
+                else {
+                    console.log('ABORT HORIZONTAL TRIGGER OPERATION');
+                    orientationRoll = 1;
+                    continue;
+                }
             }
         }
     }
